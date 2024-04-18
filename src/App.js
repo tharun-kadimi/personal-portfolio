c271345a07abc958925f9c039dd94fd48f8660a1
tharun-kadimi <tharunkadimi13@gmail.com>
2024-04-18 23:17:23 +0530

    Scripting done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import { Banner } from "./Components/Banner.js";
 import NavBarr from "./Components/NavBarr.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Skills } from "./Components/Skills.js";
+import { Projects } from "./Components/Projects.js";
+import { Footer } from "./Components/Footer.js";
+import { Contact } from "./Components/Contact.js";
 
 function App() {
   return (
     <div className="App">
       <NavBarr />
       <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
